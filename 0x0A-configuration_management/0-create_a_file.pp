44599c38b686eb6821b01b807f0a841f74f593a5
m5235 <m.ladhari@outlook.com>
2020-08-03 19:36:44 +0100

    create

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a new file
+file { 'holberton':
+    ensure  => file,
+    path    => '/tmp/holberton',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
