312fe9bc5f2fc43698e2767477d730bc962062c5
m5235 <m.ladhari@outlook.com>
2020-09-30 12:05:51 +0100

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,8 @@
 # fix 500 error
 
-exec { 'fix debug':
-  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+exec { 'fix bug':
   path    =>  '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ,
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
   
 }
