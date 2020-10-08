78ae14ca8adeea26c9b4bb529dcfd43621276be5
m5235 <m.ladhari@outlook.com>
2020-10-08 19:41:38 +0100

    task0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,3 @@
-# Puppet script
+# script of fix nginx
 exec { '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx': }
--> exec { 'service nginx restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/',
- }
+-> exec { '/usr/bin/env service nginx restart': }
