cddd8e9524516e48e4689dc5522601d8939390e2
m5235 <m.ladhari@outlook.com>
2020-11-05 11:54:45 +0100

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 
 """
-
+task 0
 """
 
-import sys
 import requests
+import sys
 
 if __name__ == "__main__":
-usr-id = sys.argv[1]
-up = requests.get(''https://jsonplaceholder.typicode.com/users/{}'.format(
-        usr-id)
-        )
-    name = up.json()
-    up = requests.get(
-     ('https://jsonplaceholder.typicode.com/todos')
-     total = 0
-     for task in todo.json():
-         if task.get('completed') 
-         done = done + 1
-            print('Employee {} is done with tasks({}/{}):'.format(name, completed, totalTasks))
+    Myurl = "https://jsonplaceholder.typicode.com/"
+
+    user_id = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos_list = requests.get(url + "todos",
+                              params={"userId": sys.argv[1]}).json()
+
+    progress = [t.get("title") for t in todos_list
+                if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(progress), len(todos_list)))
+
+    for i in progress:
+        print("\t {}".format(i))
