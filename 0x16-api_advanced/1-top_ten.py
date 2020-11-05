42561c18e646200753aff687c00534740c222dbc
m5235 <m.ladhari@outlook.com>
2020-11-05 13:20:27 +0100

    done

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+task 1
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """ prints the titles of the first 10 hot posts
+    """
+    main_url = "https://www.reddit.com"
+    headers = {
+        "User-Agent": "Ubuntu:playing with API (by /u/Cyber)"}
+    request_info = requests.get(
+        main_url + '/r/{}/hot.json?limit=10'.format(subreddit),
+        headers=headers,
+        allow_redirects=False,
+        )
+    if request_info.status_code == 404:
+        return print(None)
+    hottest = request_info.json().get("data").get("children")
+    [print(child.get("data").get("title")) for child in hottest]
+
+if __name__ == "__main__":
+    number_of_subscribers()
